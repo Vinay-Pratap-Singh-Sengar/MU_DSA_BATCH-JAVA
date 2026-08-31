fae1b3d68cb0d41da98534caab84b923d71549f7
Vinay-Pratap-Singh-Sengar <vinay.sengar17@gmail.com>
2026-08-31 20:13:49 +0530

    Basic Of Java Syntax

---
+++ src/class_1/Area.java
@@ -0,0 +1,11 @@
+package class_1;
+
+public class Area {
+    public static void main(String[] args) {
+        int length = 20;
+        int width = 30;
+        int area = length * width;
+        System.out.println(area);
+        System.out.println("Area is = " +area);
+    }
+}
--- src/class_1/First.java
+++ src/class_1/First.java
@@ -1,7 +1,31 @@
 package class_1;
 
 public class First {
+    public void greet(){
+        System.out.println("hello , good evening");
+    }
     public static void main(String[] args) {
         System.out.println("Hello World");
+        First first = new First();
+        first.greet();
     }
 }
+
+// identify the correct syntax :
+//
+////Valid Java main() method signature
+//public static void main(String[] args)
+//public static void main(String []args)
+//public static void main(String args[])
+//public static void main(String... args)
+//static public void main(String[] args)
+//public static final void main(String[] args)
+//final public static void main(String[] args)
+//final strictfp public static void main(String[] args)
+//
+//
+////    Invalid Java main() method signature
+//public void main(String[] args)
+//static void main(String[] args)
+//public void static main(String[] args)
+//abstract public static void main(String[] args)
